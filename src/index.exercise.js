e737d38f354427fdd5e46b4e9a20def36ef0ff30
Roni <roni.c.castro@gmail.com>
2021-01-02 23:59:44 -0300

    feat: Create initial bookshelf screen

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,18 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+function App() {
+  return (
+    <div>
+      <Logo width={80} height={80} />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => alert('login')}>Login</button>
+        <button onClick={() => alert('register')}>Register</button>
+      </div>
+    </div>
+  )
+}
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root'))
