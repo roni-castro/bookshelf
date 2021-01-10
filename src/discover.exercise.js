0d4c4a06e419d55690bc687a345c1fd568f017b2
Roni <roni.c.castro@gmail.com>
2021-01-09 21:44:34 -0300

    feat: Integrate book search with api

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -2,35 +2,92 @@
 import {jsx} from '@emotion/core'
 
 import './bootstrap'
+import React from 'react'
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
+
+const SearchStatus = {
+  Idle: 'idle',
+  Loading: 'loading',
+  Success: 'success',
+  Failure: 'failure',
+}
+
+const SearchActions = {
+  Search: 'books/search',
+  Loading: 'books/loading',
+  Success: 'books/success',
+  Failure: 'books/failure',
+}
+
+const initialState = {
+  status: SearchStatus.Idle,
+  data: null,
+  query: '',
+  queried: false,
+}
+
+const SearchReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case SearchActions.Search: {
+      return {
+        ...state,
+        queried: true,
+        query: action.query,
+      }
+    }
+    case SearchActions.Loading: {
+      return {
+        ...state,
+        status: SearchStatus.Loading,
+      }
+    }
+    case SearchActions.Success: {
+      return {
+        ...state,
+        status: SearchStatus.Success,
+        data: action.data,
+      }
+    }
+    case SearchActions.Failure: {
+      return {
+        ...state,
+        status: SearchStatus.Failure,
+      }
+    }
+    default: {
+      throw new Error(`action ${action} is not available`)
+    }
+  }
+}
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [{status, data, queried, query}, dispatch] = React.useReducer(
+    SearchReducer,
+    initialState,
+  )
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  React.useEffect(() => {
+    if (queried) {
+      dispatch({type: SearchActions.Loading})
+      client(`books?query=${encodeURIComponent(query)}`)
+        .then(response => {
+          dispatch({type: SearchActions.Success, data: response})
+        })
+        .catch(() => dispatch({type: SearchActions.Failure}))
+    }
+  }, [query, queried])
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  const isLoading = status === SearchStatus.Loading
+  const isSuccess = status === SearchStatus.Success
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    const query = event.target.elements['search'].value
+    dispatch({type: SearchActions.Search, query})
   }
 
   return (
