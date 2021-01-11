64d41a01b52e9d561942a533ca683028685bbc82
Roni <roni.c.castro@gmail.com>
2021-01-10 23:56:32 -0300

    feat: Init exercise extra-1

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -9,31 +9,11 @@ import {UnauthenticatedApp} from './unauthenticated-app'
 function App() {
   const [user, setUser] = React.useState(null)
 
-  async function login(formData) {
-    try {
-      const user = await auth.login(formData)
-      setUser(user)
-    } catch (err) {
-      console.warn(err)
-    }
-  }
-
-  async function register(formData) {
-    try {
-      const user = await auth.register(formData)
-      setUser(user)
-    } catch (err) {
-      console.warn(err)
-    }
-  }
-
-  async function logout() {
-    try {
-      setUser(null)
-      await auth.logout()
-    } catch (err) {
-      console.warn(err)
-    }
+  const login = form => auth.login(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setUser(u))
+  const logout = () => {
+    auth.logout()
+    setUser(null)
   }
 
   return user ? (
@@ -44,8 +24,3 @@ function App() {
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
