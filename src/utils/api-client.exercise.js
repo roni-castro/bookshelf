b2c64f4bc1aea396274b94975d947e21b6fa298d
Roni <roni.c.castro@gmail.com>
2021-01-11 00:44:17 -0300

    feat: allow client to post data

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -3,12 +3,14 @@ const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {token, headers: customHeaders, data, ...customConfig} = {},
 ) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
