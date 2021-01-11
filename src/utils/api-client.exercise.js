d2dbdb307a7edb1e3ac3b4ddf77e7e4ce651556a
Roni <roni.c.castro@gmail.com>
2021-01-11 00:36:53 -0300

    feat: Start exercise 4

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,10 +1,12 @@
 import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
+
 function client(
   endpoint,
   {token, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: 'GET',
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
       ...customHeaders,
@@ -15,8 +17,9 @@ function client(
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     if (response.status === 401) {
       await auth.logout()
+      // refresh the page for them
       window.location.assign(window.location)
-      return Promise.reject('User token expired. Please login again')
+      return Promise.reject({message: 'Please re-authenticate.'})
     }
     const data = await response.json()
     if (response.ok) {
