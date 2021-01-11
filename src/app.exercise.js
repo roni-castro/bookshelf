6ac657cb67ea00871a3fbffc69f9803ad900b27d
Roni <roni.c.castro@gmail.com>
2021-01-11 00:18:34 -0300

    feat: handle loading while fetching data

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,6 +6,9 @@ import * as auth from 'auth-provider'
 import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {useAsync} from 'utils/hooks'
+import * as colors from 'styles/colors'
+import {FullPageSpinner} from 'components/lib'
 
 async function getUser() {
   let user = null
@@ -20,21 +23,50 @@ async function getUser() {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data,
+    error,
+    isIdle,
+    isLoading,
+    isError,
+    setData,
+  } = useAsync()
 
   React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
+    getUser().then(u => setData(u))
+  }, [setData])
 
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  return data ? (
+    <AuthenticatedApp user={data} logout={logout} />
   ) : (
     <UnauthenticatedApp login={login} register={register} />
   )
