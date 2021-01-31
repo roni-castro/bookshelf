9d33ded11bcb4c7488e6895ba1bb205994c5cd47
Roni <roni.c.castro@gmail.com>
2021-01-31 19:58:09 -0300

    feat: add loading spinner to notes

--- src/screens/book.exercise.js
+++ src/screens/book.exercise.js
@@ -11,7 +11,7 @@ import {useListItem, useUpdateListItem} from 'utils/list-items'
 import {formatDate} from 'utils/misc'
 import * as mq from 'styles/media-queries'
 import * as colors from 'styles/colors'
-import {Textarea, ErrorMessage} from 'components/lib'
+import {Textarea, ErrorMessage, Spinner} from 'components/lib'
 import {Rating} from 'components/rating'
 import {StatusButtons} from 'components/status-buttons'
 
@@ -102,7 +102,7 @@ function ListItemTimeframe({listItem}) {
 }
 
 function NotesTextarea({listItem, user}) {
-  const [mutate, {error, isError}] = useUpdateListItem(user)
+  const [mutate, {error, isError, isLoading}] = useUpdateListItem(user)
   const debouncedMutate = React.useMemo(() => debounceFn(mutate, {wait: 300}), [
     mutate,
   ])
@@ -126,6 +126,7 @@ function NotesTextarea({listItem, user}) {
         >
           Notes
         </label>
+        {isLoading && <Spinner />}
         {isError ? (
           <ErrorMessage
             error={error}
