18b2d0e4357f94dc2c5d0e323ca398b71174efd1
Roni <roni.c.castro@gmail.com>
2021-03-07 20:02:57 -0300

    feat: start extra-1 prefetch the authenticated app

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import {useAuth} from './context/auth-context'
-
 import {FullPageSpinner} from './components/lib'
 
 const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
