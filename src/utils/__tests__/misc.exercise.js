9aee19967c25b5dae5d6ebd857157dbb832e10d9
Roni <roni.c.castro@gmail.com>
2021-05-30 22:18:36 -0300

    feat: add test to formatDate utils

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date(2021, 1, 10))).toBe('Feb 21')
+})
